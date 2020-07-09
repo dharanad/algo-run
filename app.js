6581c34a22ad748fcce814fe7481667b5523cab8
dharanaditya <dharan.aditya@outlook.com>
2020-07-09 14:22:47 +0530

    Serving React App from Express

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hello from AlgoRun');
+const port = process.env.PORT || 8080;
+app.use(cors()); // Same origin policy Enabled
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json())
+app.disable('x-powered-by');
+
+// Serving React App
+app.use(express.static(path.join(__dirname,'build')));
+
+
+/* Middleware */
+app.use((req, res) => {
+    res.type('text/plain');
+    res
+    .status(400)
+    .send('400 - Not Found');
 });
 
-app.listen(port, () => console.log(`Listening on PORT : ${port}`));
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.type('text/plain');
+    res
+    .status(500)
+    .send('500 - Internal Error');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on PORT : ${port}`);
+});
