901dfa9cbddc242935bd90fda65e96124a16a799
dharanaditya <dharan.aditya@outlook.com>
2020-07-09 22:07:09 +0530

    Added Output Box and Stylized UI with reactstrap

--- client/src/Selector/Selector.jsx
+++ client/src/Selector/Selector.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-
+import { Container, Row, Col } from "reactstrap";
 const Selector = (props) => {
   const list = [...props.list];
   return (
-    <div style={{ padding: "2px" }}>
-      <p style={{ display: "inline-block" }}>{props.children}:</p>
-      <select
-        onChange={(event) => props.onChange(event)}
-        style={{ display: "inline-block" }}
-      >
-        {list.map((it) => (
-          <option key={it} value={it}>
-            {it}
-          </option>
-        ))}
-      </select>
-    </div>
+    <Container>
+      <Row className="justify-content-start">
+        <Col md="2">
+          <p>{props.children}:</p>
+        </Col>
+        <Col>
+          <select onChange={(event) => props.onChange(event)}>
+            {list.map((it) => (
+              <option key={it} value={it}>
+                {it}
+              </option>
+            ))}
+          </select>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Selector;
